45946c1865fe627c50034b8aa86ca17593f82573
ashusharma890 <81765508+ashusharma890@users.noreply.github.com>
2022-07-21 22:06:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+
+btn.addEventListener("click" , () => {
+    if(input.classList.length==2)
+    {
+        input.classList.remove("active")
+    }
+    else{
+    input.classList.add("active");
+    input.focus();
+    }});
+
+// search.addEventListener("mouseleave", () => {
+//     input.classList.remove("active");
+// });
